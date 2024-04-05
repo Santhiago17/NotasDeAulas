c2a79ce3ae9b7cc2278d3af2b1a65d24eec8c35f
Santhiago <santhiagosantos7@gmail.com>
2024-04-05 09:37:42 -0300

    Resolvendo a questao 02

---
+++ src/NotaDeAula_04/Questao_02/Main.java
@@ -0,0 +1,10 @@
+package NotaDeAula_04.Questao_02;
+
+public class Main {
+    public static void main(String[] args) {
+        Produto produto1 = new Produto("Camisa", 49.99, "Camisa polo azul");
+        Produto produto2 = new Produto("Calça", 99.99, "Calça jeans preta");
+        produto1.dados();
+        produto2.dados();
+    }
+}
---
+++ src/NotaDeAula_04/Questao_02/Produto.java
@@ -0,0 +1,17 @@
+package NotaDeAula_04.Questao_02;
+
+public class Produto {
+    private String tipo, nomeProduto;
+    private double preco;
+
+    public Produto(String tipo, double preco, String nomeProduto) {
+        this.tipo = tipo;
+        this.preco = preco;
+        this.nomeProduto = nomeProduto;
+    }
+    public void dados(){
+        System.out.println("Tipo de produto: " + tipo);
+        System.out.println("Preço: " + preco);
+        System.out.println("Nome do produto: " + nomeProduto);
+    }
+}
