a2cb10d7a562133730ff196cc625c4d113c0fc71
Santhiago <santhiagosantos7@gmail.com>
2024-04-05 09:40:29 -0300

    Resolvendo a questao 04

---
+++ src/NotaDeAula_04/Questao_04/Animal.java
@@ -0,0 +1,22 @@
+package NotaDeAula_04.Questao_04;
+
+public class Animal {
+    protected String classe, locomocao;
+    public Animal(){};
+    public Animal(String classe, String locomocao){
+        this.classe = classe;
+        this.locomocao = locomocao;
+    }
+    public void setClasse(String classe){
+        this.classe = classe;
+    }
+    public void setLocomocao(String locomocao){
+        this.locomocao = locomocao;
+    }
+    public String getClasse(){
+        return classe;
+    }
+    public String getLocomocao(){
+        return locomocao;
+    }
+}
---
+++ src/NotaDeAula_04/Questao_04/Leao.java
@@ -0,0 +1,14 @@
+package NotaDeAula_04.Questao_04;
+
+public class Leao extends Animal{
+    private String pelo;
+    public Leao(String classe, String locomocao, String pelo) {
+        super(classe, locomocao);
+        this.pelo = pelo;
+    }
+    public void exibirInformacoes(){
+        System.out.println("Classe: " + classe);
+        System.out.println("Locomoção: " + locomocao);
+        System.out.println("Pelo: " + pelo);
+    }
+}
